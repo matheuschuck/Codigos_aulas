557d4de7937593c6960ae936d5c38cbe85a310fa
matheus <chuck2-mg@hotmail.com>
2022-04-27 20:27:32 -0300

    Inclusao de arquivo terminos exercicio de programacao

---
+++ triminos.py
@@ -0,0 +1,29 @@
+'''
+Problema triminos que vimos na aula de quarta feira.
+
+Enunciado:
+
+Imagine que você recebe um tabuleiro, que pode ser totalmente preenchido com
+peças chamadas triminós (apresentadas abaixo). Quando você recebe esse
+tabuleiro, te informam que uma única posição desse tabuleiro não deve ser
+preenchida com essa peça, mas todas as demais posições sim, sem exceções
+
+a) Como você resolve esse problema, sabendo que deverá escolher a posição
+correta das peças para não deixar espaços descobertos?
+
+b) Como você resolve esse problema usando divisão e conquista?
+Dados de entrada: um tabuleiro cujo tamanho é uma potência de 2; e a posição
+que não deve ser preenchida por um triminó.
+
+Saída: o tabuleiro preenchido com as peças nas posições corretas. No exemplo
+abaixo apenas algumas posições estão cobertas. Você deve apresentar uma
+solução que cubra todas.
+
+
+Para ver mais consulte:
+https://www.moodle.unisinos.br/pluginfile.php/2700396/mod_resource/content/4/exercicio_aula10.pdf
+
+O tabuleiro é de tamanho 8x8 formando 64 posições, e tem um cubo ocupado o 
+quadrado 6x e 5y.
+
+'''
