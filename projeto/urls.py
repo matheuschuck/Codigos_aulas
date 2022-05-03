6f739626518ceb9b9ba7606487b0ea4427a354e2
matheus <chuck2-mg@hotmail.com>
2022-05-02 22:29:06 -0300

    atualizações de pastas, urls, apps e views

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from recipes.views import contato, home, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 
 ]
