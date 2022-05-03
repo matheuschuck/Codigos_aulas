a2b3839f028d6aa1ac8fc966fa0fdc3455eca765
matheus <chuck2-mg@hotmail.com>
2022-05-02 22:10:46 -0300

    Alterações nas URLS

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import contato, home, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 
 ]
